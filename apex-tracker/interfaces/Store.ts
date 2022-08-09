f043a4f36de31d033241353cf1da361424da059e
kameronhayes <kodingwithkam@gmail.com>
2022-08-09 14:00:19 -0400

    update store model type

--- apex-tracker/interfaces/Store.ts
+++ apex-tracker/interfaces/Store.ts
@@ -1,2 +1,21 @@
-export type Store = {}
-export type StoreItem = {}
+export type Store = {
+    data: StoreItem[]
+}
+export type StoreItem = {
+    title: string,
+    desc: string,
+    purchaseLimit: number,
+    isAvailable: boolean,
+    expireTimestamp: number,
+    shopType: string,
+    pricing: StoreItemContent[],
+    content: StoreItemContent[],
+    offerID: string,
+    asset: string
+}
+
+export type StoreItemContent = {
+    ref: string,
+    name?: string,
+    quantity: number
+}

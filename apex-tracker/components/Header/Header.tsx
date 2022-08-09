078a956cef537c25eef714c7a1d779951e285bc3
kameronhayes <kodingwithkam@gmail.com>
2022-08-09 15:36:21 -0400

    responsive header/nav bar

---
+++ apex-tracker/components/Header/Header.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import {
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Typography,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import styles from './Header.module.css'
+import { NavItem } from '../../interfaces/NavItem'
+
+const pages: NavItem[] = [
+    {
+        name: 'Home',
+        link: '/home',
+    },
+    { name: 'News', link: '/news' },
+    { name: 'Shop', link: '/shop' },
+    { name: 'Leaderboards', link: '/leaderboards' },
+]
+
+const Header = () => {
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget)
+    }
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null)
+    }
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" className={styles.toolbar}>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        News
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                color="inherit"
+                                href={page.link}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="nav menu"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.name}>
+                                    <Button color="inherit" href={page.link}>
+                                        {page.name}
+                                    </Button>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+export default Header
